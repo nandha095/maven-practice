787c08950460bf8e62ff266a24fd7b2e0de109a1
Ubuntu <ubuntu@ip-172-31-92-87.ec2.internal>
2026-09-21 05:03:47 +0000

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven!");
+        System.out.println("Running Java application on AWS EC2.");
+    }
+}
